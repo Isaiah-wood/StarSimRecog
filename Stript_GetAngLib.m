d86f72ebe33e6aea37470ba0d956ffde2b3c9047
Isaiah <Isaiah@buaa.edu.cn>
2024-05-17 19:21:11 +0800

    生成7.5/8星等的角距库

--- Stript_GetAngLib.m
+++ Stript_GetAngLib.m
@@ -20,15 +20,15 @@ else
 end
 
 
-SubStarsLib_Mag7_5 = StarsLib(StarsLib(:, 5) <= 7.5, 1:4);
-clear StarsLib;
-AngLib_Mag7_5 = GetAngLib(AngLib_Mag7_5_Path, AngLib_Mag7_5_SortedPath, SubStarsLib_Mag7_5, fovRadius);
-
-
-
-% SubStarsLib_Mag8   = StarsLib(StarsLib(:, 5) <= 8, 1:4);
+% SubStarsLib_Mag7_5 = StarsLib(StarsLib(:, 5) <= 7.5, 1:4);
 % clear StarsLib;
-% AngLib_Mag8 = GetAngLib(AngLib_Mag8_Path, AngLib_Mag8_SortedPath, SubStarsLib_Mag8, fovRadius);
+% AngLib_Mag7_5 = GetAngLib(AngLib_Mag7_5_Path, AngLib_Mag7_5_SortedPath, SubStarsLib_Mag7_5, fovRadius);
+
+
+
+SubStarsLib_Mag8   = StarsLib(StarsLib(:, 5) <= 8, 1:4);
+clear StarsLib;
+AngLib_Mag8 = GetAngLib(AngLib_Mag8_Path, AngLib_Mag8_SortedPath, SubStarsLib_Mag8, fovRadius);
 
 
 
@@ -38,37 +38,61 @@ AngLib_Mag7_5 = GetAngLib(AngLib_Mag7_5_Path, AngLib_Mag7_5_SortedPath, SubStars
 
 
 function AngLib = GetAngLib(AngLibPath, AngLibSortedPath, StarLib, fovRadius)
-    disp(size(StarLib, 1))
-    StarLibNum = size(StarLib, 1);
-    StarIdList = StarLib(:, 1);
-    StarVecList = StarLib(:, 2:4);
+    % disp(size(StarLib, 1))
+    % StarLibNum = size(StarLib, 1);
+    % StarIdList = StarLib(:, 1);
+    % StarVecList = StarLib(:, 2:4);
+    % 
+    % LibIndex = 1;
+    % AngDistThreshold = cosd(fovRadius * 2);
+    % StarAngLib = zeros(10, 1);
+    % for i = 1:StarLibNum
+    %     Vec1 = StarVecList(i, :);
+    %     for j = i+1:StarLibNum
+    %         Vec2 = StarVecList(j, :);
+    %         AngDist = dot(Vec1, Vec2);
+    %         if AngDist > AngDistThreshold
+    %             StarAngLib(LibIndex, 1:3) = [AngDist, StarIdList(i, :), StarIdList(j, :)];
+    %             LibIndex = LibIndex + 1;
+    %         end
+    %     end
+    %     disp(i)
+    %     if mod(i, 1000) == 0 || i == StarLibNum
+    %         StarAngLib(LibIndex+1:end, :) = [];
+    %         writematrix(StarAngLib, sprintf('StarLib/temp/StarAngLib_%.5d.csv', i));
+    %         LibIndex = 1;
+    %         StarAngLib = zeros(10, 1);
+    %     end
+    % end
+    % StarAngLib(LibIndex+1:end, :) = [];
 
-    LibIndex = 1;
-    AngDistThreshold = cosd(fovRadius * 2);
-    StarAngLib = zeros(10, 1);
-    for i = 1:StarLibNum
-        Vec1 = StarVecList(i, :);
-        for j = i+1:StarLibNum
-            Vec2 = StarVecList(j, :);
-            AngDist = dot(Vec1, Vec2);
-            if AngDist < AngDistThreshold
-                StarAngLib(LibIndex, 1) = AngDist;
-                StarAngLib(LibIndex, 2) = StarIdList(i, :);
-                StarAngLib(LibIndex, 3) = StarIdList(j, :);
-                LibIndex = LibIndex + 1;
+    ImgFiles = dir('StarLib/temp/StarAngLib_*.csv');
+    if isempty(ImgFiles)
+        disp('No PNG images found in the directory.')
+    else
+        StarAngLibAll = [];
+        for i = 1:length(ImgFiles)
+            filePath = fullfile('StarLib/temp/', ImgFiles(i).name);
+            try
+                StarAngLib = readmatrix(filePath);
+            catch ME
+                disp(['Error reading ' filePath ': ' ME.message]);
+                continue;
             end
+            
+            % 删除全为0的行
+            zeroRows = all(StarAngLib == 0, 2);  
+            StarAngLib(zeroRows, :) = [];
+            
+            % 将处理后的数据存入cell数组
+            % StarAngLibAll{end+1} = StarAngLib;
+            StarAngLibAll = [StarAngLibAll; StarAngLib];
+            delete(filePath); 
         end
-        disp(i)
-        % if mod(i, 1000) == 0 || i == size(StarVecList, 1)
-        %     StarAngLib(LibIndex+1:end, :) = [];
-        %     writematrix(StarAngLib, sprintf('StarLib/StarAngLib_%.5d.csv', i));
-        %     LibIndex = 1;
-        %     StarAngLib = zeros(100, 1);
-        % end
+        StarAngLib = StarAngLibAll;
     end
-    StarAngLib(LibIndex+1:end, :) = [];
-    writematrix(StarAngLib, AngLibSortedPath);
 
-    AngLib = sortrows(StarAngLib, 1, "ascend");
+    writematrix(StarAngLib, AngLibSortedPath);
+    AngLib = sortrows(StarAngLib, 1, "descend");
     writematrix(AngLib, AngLibPath);
 end
