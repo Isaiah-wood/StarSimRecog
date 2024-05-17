a1e9e499eecae172c15015349ef2fcdec0d4286b
Isaiah <Isaiah@buaa.edu.cn>
2024-05-17 19:24:03 +0800

    小改

--- Stript_GetAngLib.m
+++ Stript_GetAngLib.m
@@ -20,9 +20,9 @@ else
 end
 
 
-% SubStarsLib_Mag7_5 = StarsLib(StarsLib(:, 5) <= 7.5, 1:4);
-% clear StarsLib;
-% AngLib_Mag7_5 = GetAngLib(AngLib_Mag7_5_Path, AngLib_Mag7_5_SortedPath, SubStarsLib_Mag7_5, fovRadius);
+SubStarsLib_Mag7_5 = StarsLib(StarsLib(:, 5) <= 7.5, 1:4);
+clear StarsLib;
+AngLib_Mag7_5 = GetAngLib(AngLib_Mag7_5_Path, AngLib_Mag7_5_SortedPath, SubStarsLib_Mag7_5, fovRadius);
 
 
 
@@ -38,33 +38,33 @@ AngLib_Mag8 = GetAngLib(AngLib_Mag8_Path, AngLib_Mag8_SortedPath, SubStarsLib_Ma
 
 
 function AngLib = GetAngLib(AngLibPath, AngLibSortedPath, StarLib, fovRadius)
-    % disp(size(StarLib, 1))
-    % StarLibNum = size(StarLib, 1);
-    % StarIdList = StarLib(:, 1);
-    % StarVecList = StarLib(:, 2:4);
-    % 
-    % LibIndex = 1;
-    % AngDistThreshold = cosd(fovRadius * 2);
-    % StarAngLib = zeros(10, 1);
-    % for i = 1:StarLibNum
-    %     Vec1 = StarVecList(i, :);
-    %     for j = i+1:StarLibNum
-    %         Vec2 = StarVecList(j, :);
-    %         AngDist = dot(Vec1, Vec2);
-    %         if AngDist > AngDistThreshold
-    %             StarAngLib(LibIndex, 1:3) = [AngDist, StarIdList(i, :), StarIdList(j, :)];
-    %             LibIndex = LibIndex + 1;
-    %         end
-    %     end
-    %     disp(i)
-    %     if mod(i, 1000) == 0 || i == StarLibNum
-    %         StarAngLib(LibIndex+1:end, :) = [];
-    %         writematrix(StarAngLib, sprintf('StarLib/temp/StarAngLib_%.5d.csv', i));
-    %         LibIndex = 1;
-    %         StarAngLib = zeros(10, 1);
-    %     end
-    % end
-    % StarAngLib(LibIndex+1:end, :) = [];
+    disp(size(StarLib, 1))
+    StarLibNum = size(StarLib, 1);
+    StarIdList = StarLib(:, 1);
+    StarVecList = StarLib(:, 2:4);
+    
+    LibIndex = 1;
+    AngDistThreshold = cosd(fovRadius * 2);
+    StarAngLib = zeros(10, 1);
+    for i = 1:StarLibNum
+        Vec1 = StarVecList(i, :);
+        for j = i+1:StarLibNum
+            Vec2 = StarVecList(j, :);
+            AngDist = dot(Vec1, Vec2);
+            if AngDist > AngDistThreshold
+                StarAngLib(LibIndex, 1:3) = [AngDist, StarIdList(i, :), StarIdList(j, :)];
+                LibIndex = LibIndex + 1;
+            end
+        end
+        disp(i)
+        if mod(i, 1000) == 0 || i == StarLibNum
+            StarAngLib(LibIndex+1:end, :) = [];
+            writematrix(StarAngLib, sprintf('StarLib/temp/StarAngLib_%.5d.csv', i));
+            LibIndex = 1;
+            StarAngLib = zeros(10, 1);
+        end
+    end
+    StarAngLib(LibIndex+1:end, :) = [];
 
     ImgFiles = dir('StarLib/temp/StarAngLib_*.csv');
     if isempty(ImgFiles)
